3c09d198cff8fd75e4eb49c7172231f9cb5a312a
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-10-04 02:05:30 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojured-csound
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojured_csound/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojured-csound.core-test
+  (:require [clojure.test :refer :all]
+            [clojured-csound.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
