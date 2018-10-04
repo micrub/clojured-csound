c80ae4b3edd696188c542997116fa5c137472f8d
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-10-04 03:56:40 +0300

    .

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clojured-csound "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :plugins  [[micrub/lein-plz "0.3.33"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha4"]])
